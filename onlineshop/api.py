450b0f93b2925f38f8a37d0a9d35e7e4ec905038
parhamsazadar <p.sazdar@gmail.com>
2020-12-23 21:46:16 +0330

    handle more api

--- onlineshop/api.py
+++ onlineshop/api.py
@@ -1,11 +1,9 @@
 import json
-
+from bson import ObjectId
 from flask import Blueprint, url_for, session, request, render_template, redirect, g, flash, jsonify
 from pymongo import MongoClient
 import io
 from json import load
-
-
 
 bp = Blueprint('api', __name__, url_prefix="/api")
 
@@ -15,10 +13,13 @@ def prod_list():
     client = MongoClient('localhost', 27017)
     db = client.online_shop
     prod_list = list(db.products.find())
+    for i in prod_list:
+        i["_id"] = str(i["_id"])
+
     return jsonify(prod_list)
 
 
-@bp.route('/product/<int:product_id>')
+@bp.route('/product/<product_id>')
 def prod_details(product_id):
     client = MongoClient('localhost', 27017)
     db = client.online_shop
@@ -26,30 +27,43 @@ def prod_details(product_id):
     return jsonify(prod_details)
 
 
-@bp.route('/product/add')
+@bp.route('/product/add', methods=('GET', 'POST'))
 def prod_add():
-    req=request.args.get('name')
-    for i in request.args:
-        print(i,':',request.args.get(i))
-    return req
+    if request.method == "POST":
+        client = MongoClient('localhost', 27017)
+        db = client.online_shop
+        prob_add = {"name_product": request.form.get('product_name'), "description": request.form.get('description'),
+                    "category": request.form.get('category'), "url_image": r"/static/images/surface.jpg"}
+        res = db.products.insert_one(prob_add)
+
+        db = list(db.products.find({"_id": res.inserted_id}))
+        for i in db:
+            i["_id"] = str(i["_id"])
+
+        return jsonify(db)
 
 
 @bp.route('/product/edit')
 def prod_edit():
-    pass
+    if request.method == "POST":
+        client = MongoClient('localhost', 27017)
+        db = client.online_shop
+        pass
 
 
-@bp.route('/product/delete/<int:product_id>')
+@bp.route('/product/delete/<product_id>')
 def prod_delete(product_id):
     client = MongoClient('localhost', 27017)
     db = client.online_shop
-    db.products.delete_one({"_id": product_id})
-    return jsonify(list(db.products.find()))
+    db.products.delete_one({"_id": ObjectId(product_id)})
+    res = list(db.products.find())
+    for i in res:
+        i["_id"] = str(i["_id"])
+    return jsonify(res)
+
 
 @bp.route('/product/json_category')
 def prod_json_category():
-    with io.open(r'onlineshop/category.json',encoding='utf-8') as f:
-        f=load(f)
-
+    with io.open(r'onlineshop/category.json', encoding='utf-8') as f:
+        f = load(f)
         return jsonify(f)
-
--- onlineshop/static/js/products.js
+++ onlineshop/static/js/products.js
@@ -1,49 +1,77 @@
 $(document).ready(function () {
     $tbody = $("#product_table")
 
+    function createRow(product) {
+        var name = product.name_product;
+        var category = product.category;
+        var image = "<img src='" + product.url_image + "' style='width: 100px' >";
+        var row = "<tr id='" + product._id + "'>";
+        row += "<td>" + image + "</td>";
+        row += "<td>" + name + "</td>";
+        row += "<td>" + category + "</td>";
+        row += "<td class='text-center'>" + "<a data-toggle=\"modal\" href=\"#Modal_edit\">ویرایش</a>" + "  " + "<a  class='delete' href=\"#\">حذف</a>" + "</td>";
+        row += "</tr>";
+        $tbody.append(row);
+    }
+    function deleteRow(product) {
+
+        res = $(document.getElementById(product._id)).find(".delete").click(function () {
+            var $tr = $(this).closest('tr');
+            url = '/api/product/delete/' + $tr.attr('id')
+            $.get(url)
+            $($tr).remove();
+
+        })
+    }
 
     $.get('/api/product/list', function (resp) {
-        resp.forEach(function (product) {
-            var name = product.name_product;
-            var category = product.category;
-            var image = "<img src='" + product.url_image + "' style='width: 100px' >";
-            var row = "<tr id='" + product._id + "'>";
-            row += "<td>" + image + "</td>";
-            row += "<td>" + name + "</td>";
-            row += "<td>" + category + "</td>";
-            row += "<td class='text-center'>" + "<a data-toggle=\"modal\" href=\"#Modal_add\">ویرایش</a>" + "  " + "<a  class='delete' href=\"#\">حذف</a>" + "</td>";
-            row += "</tr>";
-            $tbody.append(row);
+        var $resp = resp
+        $resp.forEach(product => createRow(product))
+        $resp.forEach(product => deleteRow(product))
+
+    })
+
+    $('#Modal_add #button').click(function () {
+        $.ajax({
+            url: '/api/product/add',
+            data: {
+                product_name: `${$('#Modal_add #product_name').val()}`,
+                category: `${$('#Modal_add #inputGroupSelect01').val()}`, description: `${$('#Modal_add #description').val()}`
+            },
+            type: 'POST',
+            success:
+                function (resp) {
+                    var $resp = resp
+                    $resp.forEach(product => createRow(product))
+                    $resp.forEach(product => deleteRow(product))
+                }
+        })
+    })
 
 
-
-
-
-                })
-
-
-            $.get('/api/product/json_category',function (resp) {
-                for (let i=0;resp[0].length;++i){
-                    console.log(i)
-                    console.table(resp[0][i])
-
+     $('#Modal_edit #button').click(function (){
+          $.ajax({
+            url: '/api/product/edit',
+            data: {
+                product_name: `${$('#Modal_edit #product_name').val()}`,
+                category: `${$('#Modal_edit #inputGroupSelect01').val()}`, description: `${$('#Modal_edit #description').val()}`
+            },
+            type: 'POST',
+            success:
+                function (resp) {
+                    var $resp = resp
+                    $resp.forEach(product => createRow(product))
+                    $resp.forEach(product => deleteRow(product))
                 }
 
 
-            $(".delete").click(function () {
-                var $tr = $(this).closest('tr');
-                url = '/api/product/delete/' + parseInt($tr.attr('id'))
-                $.get(url)
-                $($tr).remove();
+
+     })
 
 
-            })
 
-        })
-
-
-    })
 
 
 })
 
+})

c5bc3df9a0aaeff8d756d768f398fc72eb859c51
iman_masroori <imanmasrori@yahoo.com>
2021-01-06 00:29:23 +0330

    final version of main page and category page with sorting price

--- onlineshop/products.py
+++ onlineshop/products.py
@@ -9,50 +9,83 @@ from .manager import return_category
 bp = Blueprint('products', __name__)
 
 
+def cheep(name):
+    client = MongoClient('localhost', 27017)
+    db = client.online_shop
+    y = list(db.inventory.aggregate(
+        [{"$unwind": {"path": "$items"}}, {"$match": {"items.name_product": f"{name}"}},
+         {"$sort": {"items.price": 1}},
+         {"$limit": 1}]))
+    print("yyyyyyyyyyyyyyyyyy",y)
+    return y[0]["items"]["name_product"],y[0]["name_inventory"]
+
+
 @bp.route('/')
 def index():
     client = MongoClient('localhost', 27017)
     db = client.online_shop
     send_data = {}
-    data = {"تلفن همراه": [], "لپ تاپ": [], "تلویزیون": []}
+    data = {"گوشی": [], "لپ تاپ": [], "TV": []}
     latest_products = list(db.inventory.aggregate([
-        {"$unwind": {"path": "$items"}}, {"$sort": {"items.price": -1, "items.date_insert": 1}}]))
+        {"$unwind": {"path": "$items"}}, {"$sort": {"items.price": 1, "items.date_insert": 1}}]))
     for var in latest_products:
-        name = send_data['name_product'] = var["items"]["name_product"]
-        # send_data['date_insert'] = var["items"]["date_insert"]
         send_data['price'] = var["items"]["price"]
-        # send_data['id_product'] = var["_id"]
-        cli = list(db.products.find({"name_product": name}))
-        print(cli)
-        send_data['url_image'] = cli[0]['url_image']
-        send_data['id_product'] = cli[0]['_id']
-        send_data['cat'] = cli[0]['category']
+        name = send_data['name_product'] = var["items"]["name_product"]
+        print("namenamenmename", name)
+        print("varvarvarvar", var)
+        cheep_name,cheep_inv = cheep(name)
+        if name == cheep_name and var["name_inventory"] == cheep_inv:
 
-        if 'گوشی' in send_data['cat']:
-            data['تلفن همراه'].append(
-                [send_data['url_image'], send_data['name_product'], send_data['price'], send_data['id_product']])
-        elif 'لپ تاپ' in send_data['cat']:
-            data['لپ تاپ'].append(
-                [send_data['url_image'], send_data['name_product'], send_data['price'], send_data['id_product']])
-        elif 'tv' in send_data['cat']:
-            data['تلویزیون'].append(
-                [send_data['url_image'], send_data['name_product'], send_data['price'], send_data['id_product']])
+            cli = list(db.products.find({"name_product": name}))
+            print(cli)
+            send_data['url_image'] = cli[0]['url_image']
+            send_data['id_product'] = cli[0]['_id']
+            send_data['cat'] = cli[0]['category']
+
+            if 'گوشی' in send_data['cat']:
+                data['گوشی'].append(
+                    [send_data['url_image'], send_data['name_product'], send_data['price'], send_data['id_product']])
+            elif 'لپ تاپ' in send_data['cat']:
+                data['لپ تاپ'].append(
+                    [send_data['url_image'], send_data['name_product'], send_data['price'], send_data['id_product']])
+            elif 'TV' in send_data['cat']:
+                data['TV'].append(
+                    [send_data['url_image'], send_data['name_product'], send_data['price'], send_data['id_product']])
 
     print(data)
-
+    data = {'گوشی': data['گوشی'][0:6], 'لپ تاپ': data['لپ تاپ'][0:6], 'TV': data['TV'][0:6]}
     return render_template('/template_masroori/mainpage.html', data=data)
 
 
 @bp.route('/category/?name=<category_name>', methods=['GET', 'POST'])
 def category(category_name):
+    client = MongoClient('localhost', 27017)
+    db = client.online_shop
+    send_data = {}
+    data = {"گوشی": [], "لپ تاپ": [], "TV": []}
+    latest_products = list(db.inventory.aggregate([
+        {"$unwind": {"path": "$items"}}, {"$sort": {"items.price": 1, "items.date_insert": -1}}]))
+    lis_show = []
+    for var in latest_products:
+        name = send_data['name_product'] = var["items"]["name_product"]
 
-    # values = request.form['name2']
+        send_data['price'] = var["items"]["price"]
+
+        cli = list(db.products.find({"name_product": name}))
+        print(cli)
+        send_data['url_image'] = cli[0]['url_image']
+        send_data['id_product'] = cli[0]['_id']
+        send_data['cat'] = cli[0]['category']
+        if category_name in send_data['cat']:
+            lis_show.append(
+                [send_data['name_product'], send_data['price'], send_data['url_image'], send_data['id_product']])
+
     category_name = category_name
     lis = return_category(r'onlineshop/category.json')
     lis_goshi = []
     lis_tv = []
     lis_lop = []
-    print("||||||||||||||||||||||||||||",category_name)
+
     for item in lis:
         if 'کالای دیجیتال/گوشی' in item:
             lis_goshi.append(item.split('/')[2])
@@ -62,14 +95,10 @@ def category(category_name):
     for item in lis:
         if 'کالای دیجیتال/لپ تاپ' in item:
             lis_lop.append(item.split('/')[2])
-    lis_prod = {"تلفن همراه": lis_goshi, "تلویزیون": lis_tv, "لپ تاپ": lis_lop}
-    if request.method == 'POST':
-        values = json.dumps(request.form['name2'])
-        print("=======+++++++++values+++++++>>>.", values)
-        return render_template('template_masroori/new_products.html', values=values, lis_prod=lis_prod,
-                               category_name=category_name)
+    lis_prod = {"گوشی": lis_goshi, "TV": lis_tv, "لپ تاپ": lis_lop}
+
     return render_template('template_masroori/new_products.html', lis_prod=lis_prod,
-                           category_name=category_name)
+                           category_name=category_name, lis_show=lis_show)
 
 
 @bp.route('/product/<product_id>')
